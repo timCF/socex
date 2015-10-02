5df1542db3f2b3b9715742ec0f367f01ae6c4ca3
tim <tim@MacBook-Pro-tim.local>
2015-10-02 10:52:34 +0300

    chat works

--- lib/api.ex
+++ lib/api.ex
@@ -1,7 +1,8 @@
 defmodule Socex.Api do
 	use Silverb, 	[
 						{"@ttl", Application.get_env(:socex, :update_ttl)},
-						{"@show_commands", ["ls"]}
+						{"@show_commands", ["ls", "help"]},
+						{"@mess_count", 200}
 					]
 	use ExActor.GenServer, export: :vk_api
 	use Httphex,  [
@@ -32,9 +33,28 @@ defmodule Socex.Api do
 			^dialogs ->
 				{:noreply, fullstate, @ttl}
 			new_dialogs when is_list(new_dialogs) -> 
-				Socex.notice("got new dialogs")
-				{:noreply, HashUtils.set(fullstate, [dialogs: new_dialogs, stamp: Exutils.makestamp]) |> store, @ttl}
+				Socex.Shell.draw_dialogs_list(new_dialogs)
+				{:noreply, HashUtils.set(fullstate, [dialogs: new_dialogs, stamp: Exutils.makestamp]), @ttl}
 		end
+		|> store
+	end
+	definfo :timeout, state: fullstate = %Socex{state: "dialog", current_dialog: current_dialog, messages: messages} do
+		case messages_list(current_dialog) do
+			{:error, error} -> 
+				Socex.error("error due loading messages list #{inspect error}")
+				{:noreply, HashUtils.set(fullstate, [stamp: Exutils.makestamp]), @ttl}
+			^messages ->
+				{:noreply, fullstate, @ttl}
+			lst when is_list(lst) -> 
+				Socex.Shell.draw_messages_list(lst)
+				{:noreply, HashUtils.set(fullstate, [messages: lst, stamp: Exutils.makestamp]), @ttl}
+		end
+		|> store
+	end
+	defcall command(scom), when: (scom in @show_commands), state: fullstate = %Socex{}, timeout: 60000 do
+		process_show_command(fullstate, scom)
+		{:reply, :ok, fullstate, timeout(fullstate)}
+		|> store
 	end
 	defcall command(cmd), when: is_binary(cmd), state: fullstate = %Socex{state: "menu", dialogs: dialogs}, timeout: 60000 do
 		case Maybe.to_integer(cmd) do
@@ -45,28 +65,41 @@ defmodule Socex.Api do
 				end
 			"" ->
 				{:reply, :ok, fullstate, timeout(fullstate)}
-			scom when (scom in @show_commands) ->
-				process_show_command(fullstate, scom)
-				{:reply, :ok, fullstate, timeout(fullstate)}
 			_ ->
 				case Enum.filter(dialogs, fn(%{title: title}) -> String.contains?(title, cmd) end ) do
 					[] -> {:reply, :ok, fullstate, timeout(fullstate)}
 					[subj|_] -> swith_to_dialog(fullstate, subj)
 				end
 		end
+		|> store
 	end
-	defp swith_to_dialog(fullstate = %Socex{}, subj) do
-		IO.inspect(subj)
-		#
-		#	TODO
-		#
+	defcall command("cd .."), state: fullstate = %Socex{state: "dialog"}, timeout: 60000 do
+		{:reply, :ok, HashUtils.set(fullstate, [state: "menu"]), timeout(fullstate)}
+		|> store
+	end
+	defcall command(""), state: fullstate = %Socex{}, timeout: 60000 do
 		{:reply, :ok, fullstate, timeout(fullstate)}
+		|> store
+	end
+	defcall command(mess), state: fullstate = %Socex{state: "dialog"}, timeout: 60000 do
+		case send_message(mess, fullstate) do
+			:ok -> :ok
+			{:error, error} -> Socex.error("error due sending message #{inspect error}")
+		end
+		{:reply, :ok, fullstate, timeout(fullstate)}
+		|> store
 	end
 
+	defp swith_to_dialog(fullstate = %Socex{}, subj), do: {:reply, :ok, HashUtils.set(fullstate, [current_dialog: subj, state: "dialog"]), timeout(fullstate)}
+
+	defp store({:reply, reply, state = %Socex{}, time}), do: {:reply, reply, state |> store, time}
+	defp store({:noreply, state = %Socex{}, time}), do: {:noreply, state |> store, time}
 	defp store(state = %Socex{}), do: Socex.Tinca.put(state, :curstate, :curstate)
 	def cur_state, do: Socex.Tinca.get(:curstate, :curstate)
 
+	defp process_show_command(%Socex{}, "help"), do: Socex.Shell.help
 	defp process_show_command(%Socex{state: "menu", dialogs: dialogs}, "ls"), do: Socex.Shell.draw_dialogs_list(dialogs)
+	defp process_show_command(%Socex{state: "dialog", messages: messages}, "ls"), do: Socex.Shell.draw_messages_list(messages)
 
 	#
 	#	to vk api
@@ -86,7 +119,8 @@ defmodule Socex.Api do
 				{:error, some}
 		end
 	end
-	defp get_user_name(uid) when is_integer(uid) do
+	defp get_user_name(uid, default \\ nil)
+	defp get_user_name(uid, default) when is_integer(uid) do
 		case Socex.Tinca.get(uid, :users_names) do
 			bin when is_binary(bin) -> 
 				bin
@@ -94,19 +128,46 @@ defmodule Socex.Api do
 				sleep
 				case %{access_token: Socex.Storage.vk_token, user_ids: to_string(uid)} |> http_get(["users.get"]) do
 					%{response: [%{first_name: n1, last_name: n2, uid: uid}]} -> Socex.Tinca.put("#{n1} #{n2}", uid, :users_names)
-					_ -> nil
+					_ -> default
 				end
 		end
 	end
+
+
 	defp messages_list(%{chat_id: chat_id}) do
 		sleep
-		%{access_token: Socex.Storage.vk_token, chat_id: chat_id}
+		%{access_token: Socex.Storage.vk_token, chat_id: chat_id, count: @mess_count}
 		|> http_get(["messages.getHistory"])
+		|> parse_mess_list
 	end
 	defp messages_list(%{uid: uid}) do
 		sleep
-		%{access_token: Socex.Storage.vk_token, user_id: uid}
+		%{access_token: Socex.Storage.vk_token, user_id: uid, count: @mess_count}
 		|> http_get(["messages.getHistory"])
+		|> parse_mess_list
 	end
+	defp parse_mess_list(%{response: [_|lst = [_|_]]}) do
+		Enum.reduce(lst, [], fn
+			el = %{from_id: uid, body: body, date: stamp}, acc when (is_integer(uid) and (uid >= 0) and is_binary(body) and is_integer(stamp) and (stamp >= 0)) -> [%{user: get_user_name(uid, to_string(uid)), body: String.strip(body), date: :timer.seconds(stamp) + :timer.hours(3), att: Map.get(el, :attachment), resend: Map.get(el, :fwd_messages)}|acc]
+			_, acc -> acc
+		end)
+	end
+	defp parse_mess_list(some), do: {:error, some}
+
+
+	defp send_message(bin, %Socex{current_dialog: %{chat_id: chat_id}}) when is_binary(bin) do
+		sleep
+		%{access_token: Socex.Storage.vk_token, chat_id: chat_id, message: bin}
+		|> http_get(["messages.send"])
+		|> parse_mess_req
+	end
+	defp send_message(bin, %Socex{current_dialog: %{uid: user_id}}) when is_binary(bin) do
+		sleep
+		%{access_token: Socex.Storage.vk_token, user_id: user_id, message: bin}
+		|> http_get(["messages.send"])
+		|> parse_mess_req
+	end
+	defp parse_mess_req(%{response: int}) when (is_integer(int) and (int >= 0)), do: :ok
+	defp parse_mess_req(some), do: {:error, some}
 
 end
--- lib/shell.ex
+++ lib/shell.ex
@@ -1,11 +1,13 @@
 defmodule Socex.Shell do
 	use Silverb, 	[
+						{"@inverse", IO.ANSI.inverse},
 						{"@green", IO.ANSI.green},
 						{"@cyan", IO.ANSI.cyan},
 						{"@yellow", IO.ANSI.yellow},
 						{"@magenta", IO.ANSI.magenta},
 						{"@blue", IO.ANSI.blue},
-						{"@bright", IO.ANSI.bright}
+						{"@bright", IO.ANSI.bright},
+						{"@all_colors", [IO.ANSI.cyan, IO.ANSI.yellow, IO.ANSI.magenta, IO.ANSI.blue, IO.ANSI.bright, IO.ANSI.magenta_background, IO.ANSI.underline, IO.ANSI.white, IO.ANSI.blue_background]}
 					]
 
 	def go, do: ((IO.gets(prompt) |> String.strip |> Socex.Api.command); go)
@@ -16,19 +18,49 @@ defmodule Socex.Shell do
 		end
 	end
 
+	def draw_dialogs_list([]), do: :ok
 	def draw_dialogs_list(lst) do
 		Stream.with_index(lst)
 		|> Stream.map(fn({%{title: title}, index}) -> "#{col(index, @cyan)} #{col(title, @yellow)}" end)
 		|> Enum.join("\n")
 		|> IO.puts
 	end
+	def draw_messages_list([]), do: :ok
+	def draw_messages_list(lst) do
+		members_cols = 	Stream.map(lst, fn(%{user: user}) -> user end)
+						|> Stream.uniq 
+						|> Stream.with_index
+						|> Enum.reduce(%{}, fn({user, index}, acc) -> 
+							case Enum.at(@all_colors, index) do
+								nil -> Map.put(acc, user, @cyan)
+								color -> Map.put(acc, user, color)
+							end
+						end)
+		Stream.map(lst, fn(%{user: user, body: body, date: date, att: att, resend: resend}) -> 
+			user_col = Map.get(members_cols, user)
+			"#{col( Exutils.timestamp_to_datetime(date) |> Exutils.prepare_verbose_datetime, @green)} : #{ col(user, user_col) }"
+			|> maybe_add_body(body, user_col)
+			|> maybe_add_att(att)
+			|> maybe_add_resend(resend)
+		end)
+		|> Enum.join("\n")
+		|> IO.puts
+	end
 	defp col(some, col), do: "#{IO.ANSI.reset}#{col}#{some}#{IO.ANSI.reset}"
 
+	defp maybe_add_body(str, "", _), do: str
+	defp maybe_add_body(str, body, color), do: str<>" : #{col(body, color)}"
+
+	defp maybe_add_att(str, nil), do: str
+	defp maybe_add_att(str, att), do: str<>" : #{inspect att}"
+
+	defp maybe_add_resend(str, nil), do: str
+	defp maybe_add_resend(str, resend), do: str<>" : #{inspect(resend) |> col(@inverse)}"
+
 	def help do
-		colors = [@green, @cyan, @yellow, @magenta, @blue, @bright]
-		"\nHello, user. This is crazy console VK chat. Special commands: [ls]. Enjoy!\n"
+		"\nHello, user. This is crazy console VK chat. Special commands: 'ls' 'cd ..' 'help'. Enjoy!\n"
 		|> String.split(" ")
-		|> Stream.map(&(col(&1, Enum.shuffle(colors) |> List.first)))
+		|> Stream.map(&(col(&1, Enum.shuffle(@all_colors) |> List.first)))
 		|> Enum.join(" ")
 		|> IO.puts
 	end
